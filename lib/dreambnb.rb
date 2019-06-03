05cc067a7016360cb2358546361791aff440b3d1
Student <student@Makerss-MacBook-Pro-3.local>
2019-06-03 16:37:12 +0100

    DataMapper setup added

---
+++ lib/dreambnb.rb
@@ -0,0 +1,18 @@
+require 'data_mapper'
+require 'dm-postgres-adapter'
+require_relative './user'
+
+
+class Listing
+  include DataMapper::Resource
+
+  property :prop_id,             Serial
+  property :user_id,             String, :required => true  
+  property :name,                String, :required => true  
+  property :description,         String, :required => true 
+  property :price,               String, :required => true 
+  property :available_from,      DateTime,      
+  property :available_to,        DateTime, 
+
+  belongs_to :user, :required => false
+end
---
+++ lib/user.rb
@@ -0,0 +1,13 @@
+require 'data_mapper'
+require 'dm-postgres-adapter'
+require_relative './dreambnb'
+
+class User
+  include DataMapper::Resource
+
+  property :id,             Serial
+  property :email,          String, :required => true, :unique => true
+  property :password,       String, :required => true
+  
+  has n, :listing
+end
